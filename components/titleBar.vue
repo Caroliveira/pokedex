d7b1c52e4c7a0bd8fe8fcda51e4fc09ae9c9db32
Carolina Oliveira <caroliveira.ti@gmail.com>
2020-07-27 16:29:22 -0300

    default layout and title bar

---
+++ components/titleBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row class="mb-6">
+    <v-col cols="12" md="8">
+      <h1 class="primary-title">Pokedex</h1>
+    </v-col>
+    <v-col cols="12" md="4">
+      <v-text-field
+        solo
+        flat
+        rounded
+        clearable
+        hide-details
+        placeholder="Search Pokemon"
+        prepend-inner-icon="mdi-magnify"
+        background-color="#F5F5F5"
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" src="~/assets/scss/titleBar.scss" scoped />
